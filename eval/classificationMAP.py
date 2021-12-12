2caf8892c2732a08fd727e708ab6694bc55e9f46
kiwi_fung <fjchange@hotmail.com>
2021-12-12 10:52:11 +0800

    Update classificationMAP.py

--- eval/classificationMAP.py
+++ eval/classificationMAP.py
@@ -4,7 +4,7 @@ def getAP(conf,labels):
     assert len(conf)==len(labels)
     sortind = np.argsort(-conf)
     tp = labels[sortind]==1; fp = labels[sortind]!=1
-    npos = np.sum(labels);
+    npos = np.sum(labels)
 
     fp = np.cumsum(fp).astype('float32'); tp = np.cumsum(tp).astype('float32')
     rec=tp/npos; prec=tp/(fp+tp)
